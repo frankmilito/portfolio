c21669c956f1ab1159f92287849247953dd6f5f1
frankmilito <frankobidike20@gmail.com>
2020-08-07 22:27:39 -0600

    Added scroll indicator, footer, hamburger menu responsive

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ var nav = document.getElementById("nav");
 var exit = document.getElementById("exit");
 
 menu.addEventListener("click", function (e) {
-  nav.classList.toggle("menu-items");
+  // nav.classList.toggle("menu-items");
+  nav.style.width = "60%";
   e.preventDefault();
 });
 exit.addEventListener("click", function (e) {
-  nav.classList.add("menu-items");
+  // nav.classList.add("menu-items");
+  nav.style.width = "0";
   e.preventDefault();
 });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
